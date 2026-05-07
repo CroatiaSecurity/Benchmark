315791e1cd7ad20284c29d36c2aa1235f2a24369
tandrlemandrle <tandrlemandrle@example.com>
2026-05-07 19:57:19 +0200

    Initial commit - all local content

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
-using System;
+﻿using System;
 using System.Windows.Forms;
 
 namespace GorstakBenchmark
@@ -43,49 +42,3 @@ namespace GorstakBenchmark
         }
     }
 }
-=======
-using System;
-using System.Windows.Forms;
-
-namespace GorstakBenchmark
-{
-    static class Program
-    {
-        [STAThread]
-        static void Main()
-        {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-
-            Application.ThreadException += (s, e) =>
-            {
-                try
-                {
-                    MessageBox.Show(e.Exception.ToString(), "Gorstak Benchmark - Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                }
-                catch { }
-            };
-
-            AppDomain.CurrentDomain.UnhandledException += (s, e) =>
-            {
-                try
-                {
-                    var ex = e.ExceptionObject as Exception;
-                    MessageBox.Show(ex != null ? ex.ToString() : e.ExceptionObject.ToString(),
-                        "Gorstak Benchmark - Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                }
-                catch { }
-            };
-
-            try
-            {
-                Application.Run(new MainForm());
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.ToString(), "Gorstak Benchmark - Startup Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-        }
-    }
-}
->>>>>>> 3d3535ab3f0cc7ffda6c31d1b5ec2b6adbb77c7e
